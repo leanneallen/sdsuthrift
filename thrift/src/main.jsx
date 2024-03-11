ae9df0f8726c10aa53c6d00dd7f1df89ce1b9f28
jambho <jamal.bhola@gmail.com>
2024-03-10 19:43:00 -0700

    Revert "Initialization of various libraries"
    This reverts commit b0e6b9b1fdb3c053d3538d0d4427718f40019f39.

--- thrift/src/main.jsx
+++ thrift/src/main.jsx
@@ -1,33 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import Root from "./pages/Homepage.jsx"
-import ListingPortal from './pages/ListingsPortal.jsx'
-import ErrorPage from "./error-page.jsx"
 import './index.css'
-
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import "./index.css";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element:<Root />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "ListingPortal/:contactId",
-    element: <ListingPortal />,
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <RouterProvider router={router} />
-
   </React.StrictMode>,
 )
